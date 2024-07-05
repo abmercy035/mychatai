cbdd4f0741838cc47e828fe25087361db060a981
Abraham Samuel {JavaScript Enthusiast} <abmercy035@gmail.com>
2024-07-05 14:29:46 +0100

    moved login to root / from /login

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/no-children-prop */
-import { useState } from 'react'
 import './App.css'
 import Layout from './onboarding/Layout'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './onboarding/Login'
 import Signup from './onboarding/Signup'
 import MainChat from './chatpage/MainChat'
@@ -14,7 +13,7 @@ function App() {
       <UserProvider>
         <BrowserRouter>
           <Routes>
-            <Route exact path="/login" element={<Layout children={<Login />} />} />
+            <Route exact path="/" element={<Layout children={<Login />} />} />
             <Route exact path="/signup" element={<Layout children={<Signup />} />} />
             <Route exact path="/chat" element={<MainChat />} />
           </Routes>
